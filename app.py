95d5abd344316f5187a07773fdf0ec52d9e4e625
Alexander Mies <63265718+AlexanderMies@users.noreply.github.com>
2022-10-17 15:17:23 -0400

    Add files via upload
    python script to be run in in the "years" dir of backup recordings to change the folder name to "Title" from corresponding xml's

---
+++ app.py
@@ -0,0 +1,38 @@
+
+from xml.dom import minidom
+import xml.etree.ElementTree as ET
+import os
+import shutil
+
+#for every dir in YEAR
+	#this dir's name = this dir's xml.title
+
+
+# this was the path on one of the CVN machines, 
+# but the path this should execute in might be 
+# smb://128.59.106.65/cvnsyn_45b/Course Backups/2021 or something
+path = '/Users/cvn-cc-9/Desktop/testEnv/2023/'
+for folder, subfolders, files in os.walk(path):
+	if folder != path:
+		for f in files:
+			if f.endswith(".xml"):
+				xmlPath = os.path.join(folder,f)
+				#pull DOM from xml
+				dom = minidom.parse(xmlPath)
+				#pull title from DOM
+				title = dom.getElementsByTagName('Title')
+				#get text in title and clean
+				intendedName = title[0].firstChild.nodeValue
+				intendedName = intendedName.replace('/','.')
+
+				#make string that will be final name (usually course num, section, name, date) but some are different
+				fullPath = path+intendedName
+				#replace old filename with new file name
+				shutil.move(folder,fullPath)
+
+
+
+
+
+
+
